2430a8cee48d2ad40b0a0ffcda4d3d514997e736
JosueButron <82522767+JosueButron@users.noreply.github.com>
2023-06-05 17:21:07 -0500

    rediseño
    Rediseño de la navbar solo index

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+function openNav(){
+    document.getElementById("mobile-menu").style.width ="100%";
+}
+function closeNav(){
+    document.getElementById("mobile-menu").style.width ="0%";
+}
+
+window.addEventListener('scroll', function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY>0);
+})
